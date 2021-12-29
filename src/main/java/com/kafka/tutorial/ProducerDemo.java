2fdaa7bdc61e9c12d9f963aa5367b0b7a33a4f3e
PrashantKolhe <kolheprashant204@gmail.com>
2021-12-28 20:14:22 -0800

    Created Kafka Producer

--- src/main/java/com/kafka/tutorial/ProducerDemo.java
+++ src/main/java/com/kafka/tutorial/ProducerDemo.java
@@ -1,8 +1,35 @@
 package com.kafka.tutorial;
 
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World !");
+
+        String bootStrapServers="localhost:9092";
+
+        //create producer properties
+        Properties properties=new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Create the producer
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        //Create producer record
+        ProducerRecord<String,String> record =new ProducerRecord<>("first_topic","hello world");
+
+        //send data-async
+        producer.send(record);
+
+        //flush data
+        producer.flush();
+
+        //flush and close data
+        producer.close();
     }
 }
